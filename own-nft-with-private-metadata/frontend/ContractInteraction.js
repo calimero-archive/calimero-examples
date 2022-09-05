a7192eea6e3e69455ae579920db493bd79cf3aec
matej.vukosav <matej.vukosav@gmail.com>
2022-09-05 11:02:47 +0200

    add near-api-js

---
+++ own-nft-with-private-metadata/frontend/ContractInteraction.js
@@ -0,0 +1,16 @@
+
+function initContract () {
+    console.log( "init" );
+}
+
+function mintContract () {
+    console.log( "mint" );
+}
+
+function ViewContract () {
+    console.log( "view" );
+}
+
+function installContract(){
+    console.log("install");
+}
