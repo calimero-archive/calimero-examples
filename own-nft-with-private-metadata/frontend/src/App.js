e71a540e27045b765c9be86dd8bbd7fa7e438594
chefsale <sandi.fatic@gmail.com>
2022-09-05 12:16:19 +0100

    basic frontend

--- own-nft-with-private-metadata/frontend/src/App.js
+++ own-nft-with-private-metadata/frontend/src/App.js
@@ -1,25 +1,119 @@
 import logo from './logo.svg';
 import './App.css';
+/* This example requires Tailwind CSS v2.0+ */
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import RegistryRecordsTable from './components/RegistryRecordsTable';
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Route, Routes,
+} from "react-router-dom";
+
+const navigation = [
+  { name: 'Government', href: '#', current: true },
+  { name: 'User', href: 'user', current: false },
+  { name: 'Marketplace', href: 'marketplace', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={< Home />}/>
+        <Route path="/user" element={< Home />}/>
+        <Route path="/marketplace" element={< Home />}/>
+      </Routes>
+  </Router>
   );
+}
+
+function Home() {
+  return (
+        <body class="h-full">
+      <div className="min-h-full">
+        <Disclosure as="nav" className="border-b border-gray-200 bg-white">
+          {({ open }) => (
+            <>
+              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="flex h-16 justify-between">
+                  <div className="flex">
+                    <div className="flex flex-shrink-0 items-center">
+                      <img
+                        className="block h-8 w-auto lg:hidden"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt="Your Company"
+                      />
+                      <img
+                        className="hidden h-8 w-auto lg:block"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                        alt="Your Company"
+                      />
+                    </div>
+                    <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? 'border-indigo-500 text-gray-900'
+                              : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                            'inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                          )}
+                          aria-current={item.current ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <Disclosure.Panel className="sm:hidden">
+                <div className="space-y-1 pt-2 pb-3">
+                  {navigation.map((item) => (
+                    <Disclosure.Button
+                      key={item.name}
+                      as="a"
+                      href={item.href}
+                      className={classNames(
+                        item.current
+                          ? 'bg-indigo-50 border-indigo-500 text-indigo-700'
+                          : 'border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800',
+                        'block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                      )}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </Disclosure.Button>
+                  ))}
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+
+        <div className="py-10">
+          <header>
+          </header>
+          <main>
+            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+              {/* Replace with your content */}
+              <div className="px-4 py-8 sm:px-0">
+                <RegistryRecordsTable></RegistryRecordsTable>
+              </div>
+              {/* /End replace */}
+            </div>
+          </main>
+        </div>
+      </div>
+      </body>
+  )
 }
 
 export default App;
