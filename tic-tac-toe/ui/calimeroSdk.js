a9b374501db3403e5068e03a37615d2bbf1f020e
Fran Domovic <fran@calimero.network>
2022-09-06 10:52:16 +0200

    integrated wallet

--- tic-tac-toe/ui/calimeroSdk.js
+++
@@ -1,8 +0,0 @@
-import { CalimeroSdk } from "calimero-auth-sdk";
-
-export default CalimeroSdk.init({
-  shardId: "hackaton-calimero-testnet",
-  walletUrl: "https://localhost:1234",
-  calimeroUrl: "https://api.development.calimero.network",
-  calimeroWebSdkService: "https://api.development.calimero.network",
-});
