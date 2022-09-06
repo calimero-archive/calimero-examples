50f0f35ffb6270c0068a50dbdffa0aac26cc1362
matej.vukosav <matej.vukosav@gmail.com>
2022-09-06 12:14:33 +0200

    refactor react to nextjs

---
+++ own-nft-with-private-metadata/frontend/pages/government/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RegistryRecordsTable from "./RegistryRecordsTable";
+
+export default function Government() {
+    return (
+        <div>
+            <div className="px-4 py-8 sm:px-0" >
+                <RegistryRecordsTable />
+            </div>
+        </div>
+    );
+}
---
+++ own-nft-with-private-metadata/frontend/pages/user/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserRecordsTable from "./UserRecordsTable";
+
+export default function UserData() {
+    return (
+        <div>
+            <div className="px-4 py-8 sm:px-0" >
+                <UserRecordsTable />
+            </div>
+        </div>
+    );
+}
