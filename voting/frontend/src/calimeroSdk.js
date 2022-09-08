02d14c71cc46675c973cd4e5e4b958e9144a82ed
medicz <zvonimir.medic@gmail.com>
2022-09-08 11:04:34 +0200

    Voting WIP (#1)
    * Initial private side contract
    * Add build.sh
    * frontend start
    
    Co-authored-by: Ivan Soldo <ivan@calimero.network>

---
+++ voting/frontend/src/calimeroSdk.js
@@ -0,0 +1,8 @@
+import { CalimeroSdk } from "calimero-auth-sdk";
+
+export default CalimeroSdk.init({
+    shardId: "hackathon-calimero-testnet",
+    walletUrl: "https://localhost:1234",
+    calimeroUrl: "https://api.development.calimero.network",
+    calimeroWebSdkService: "https://api.development.calimero.network",
+});
