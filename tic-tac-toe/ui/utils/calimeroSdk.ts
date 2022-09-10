cfa3a00fe486ab01b61e4c3461a0322e1494519b
Xabi Losada <xabi@calimero.network>
2022-09-10 01:28:37 +0100

    feat: update wallet url

--- tic-tac-toe/ui/utils/calimeroSdk.ts
+++ tic-tac-toe/ui/utils/calimeroSdk.ts
@@ -2,7 +2,7 @@ import { CalimeroSdk } from "calimero-auth-sdk";
 
 export default CalimeroSdk.init({
   shardId: "h15-calimero-testnet",
-  walletUrl: "https://localhost:1234",
+  walletUrl: "https://testnet.mynearwallet.com",
   calimeroUrl: "https://api.development.calimero.network",
   calimeroWebSdkService: "https://api.development.calimero.network",
 });
