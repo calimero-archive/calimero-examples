8a300c3f369bca233322842b16ed40375c85ceaa
Fran Domovic <fran@calimero.network>
2022-12-21 13:42:39 +0100

    feat: example NH side menu

--- tic-tac-toe/ui/components/nh/pageWrapper/PageWrapper.tsx
+++ tic-tac-toe/ui/components/nh/pageWrapper/PageWrapper.tsx
@@ -1,14 +1,36 @@
+import Head from "next/head";
+import { ReactNode } from "react";
 import Navigation from "../navigation/Navigation";
+import { SideNavigation } from "../sideNavigation/SideNavigation";
 
-export default function PageWrapper() {
+interface PageWrapperProps {
+  currentPage: string;
+  title: String;
+  children: ReactNode;
+}
+
+export default function PageWrapper({
+  currentPage,
+  title,
+  children,
+}: PageWrapperProps) {
   return (
-    <div className="flex flex-col w-screen bg-nh-bglight items-center min-h-screen py-8">
-      <div className="w-full max-w-nh">
-        <Navigation />
+    <>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex flex-col w-screen bg-nh-bglight items-center min-h-screen py-8">
+        <div className="w-full max-w-nh">
+          <Navigation />
+        </div>
+        <div className="w-full max-w-nh flex">
+          <div className="w-1/4">
+            <SideNavigation menuPage={currentPage} />
+          </div>
+          <div className="w-3/4">{children}</div>
+        </div>
       </div>
-      <div className="w-3/4 h-full max-w-nh-content-width bg-green-500">
-        {/*TO DO CONTENT WRAPPER */}
-      </div>
-    </div>
+    </>
   );
 }
