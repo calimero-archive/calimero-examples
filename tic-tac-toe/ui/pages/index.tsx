eae9db393d4e810ab026b251ec1d4f13b42c3ca4
Fran Domovic <fran@calimero.network>
2022-12-21 13:14:32 +0100

    feat: example NH navigation bar

--- tic-tac-toe/ui/pages/index.tsx
+++ tic-tac-toe/ui/pages/index.tsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import LoginComponent from "../components/dashboard/LoginComponent";
 import OpenGamesList from "../components/dashboard/OpenGameList";
 import MenuNavigation from "../components/Navigation";
+import CalimeroLogo from "../components/nh/images/CalimeroLogo";
+import TictactoeLogo from "../components/nh/images/TictactoeLogo";
+import PageWrapper from "../components/nh/pageWrapper/PageWrapper";
 import calimeroSdk from "../utils/calimeroSdk";
 
 export default function Dashboard() {
@@ -15,32 +18,5 @@ export default function Dashboard() {
       setLogged(false);
     }
   });
-  return (
-    <div>
-      <Head>
-        <title>Dashboard | Calimero</title>
-        <meta name="description" content="TicTacToe" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <MenuNavigation />
-      <main className="h-screen w-full ">
-        <div className=" pt-24 px-32 flex justify-between items-center">
-          <div className="text-5xl font-bold ">
-            <p>TIC-TAC-TOE - Current Games</p>
-          </div>
-        </div>
-        <div className="mt-1">
-          {logged ? (
-            <div>
-              <OpenGamesList />
-            </div>
-          ) : (
-            <div>
-              <LoginComponent />
-            </div>
-          )}
-        </div>
-      </main>
-    </div>
-  );
+  return <PageWrapper />;
 }
