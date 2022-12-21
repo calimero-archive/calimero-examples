521e2153b9de2e941937bc1373ab603bbcef4d20
Fran Domovic <fran@calimero.network>
2022-12-21 16:34:39 +0100

    feat: tictactoe game board design

--- tic-tac-toe/ui/pages/currentgames.tsx
+++ tic-tac-toe/ui/pages/currentgames.tsx
@@ -1,13 +1,21 @@
 import { useRouter } from "next/router";
-import CurrentGames from "../components/nh/currentGamesPage/CurrentGames";
+import GameBoard from "../components/nh/gameBoard/GameBoard";
 import PageWrapper from "../components/nh/pageWrapper/PageWrapper";
+
+const gameData = {
+  boardStatus: ["X", "X", "O", "X", "O", "U", "U", "U", "U"],
+  playerA: "xabi.calimero",
+  playerB: "fran.calimero",
+  playerTurn: "xabi.calimero",
+  status: "playerAwon",
+};
 
 export default function CurrentGamesPage() {
   const router = useRouter();
 
   return (
     <PageWrapper title={"PropUrl"} currentPage={router.pathname}>
-      <CurrentGames />
+      <GameBoard gameData={gameData} gameId={1} />
     </PageWrapper>
   );
 }
