8f533c66460b6669454c7c51fd28079b7e261f69
Igor Jerkovic <igor@calimero.network>
2023-03-01 18:17:49 +0100

    Using the calimero-sdk-rs for xsc connectors (#21)
    Test plan:
    Verified tictactoe game can be started from NEAR on Calimero and callback
    is received on NEAR with the new macro lib that simplifies boilerplate code.
    
    Verified game_started callback executed on NEAR after execution on Calimero:
    https://explorer.testnet.near.org/transactions/G9UV4QoJGcmcWy4G1rCdmjm11xYKyacFnhuWypvNhpqG

--- tic-tac-toe/contracts/calimero-tictactoe/deploy_calimero.sh
+++ tic-tac-toe/contracts/calimero-tictactoe/deploy_calimero.sh
@@ -5,16 +5,16 @@ if [ "$#" -ne 1 ]; then
 fi
 
 shard_id=$1
-destination_master_account="testnet"
+destination_master_account="$shard_id.calimero.testnet"
 
 near create-account \
   "tictactoe.$destination_master_account" \
   --masterAccount $destination_master_account \
-  --nodeUrl "https://api.development.calimero.network/api/v1/shards/$shard_id-calimero-testnet/neard-rpc/" \
+  --nodeUrl "https://api.dev.calimero.network/api/v1/shards/$shard_id-calimero-testnet/neard-rpc/" \
   --networkId "$1-calimero-testnet" && \
 near deploy \
   --accountId "tictactoe.$destination_master_account" \
   --initFunction new --initArgs {} \
   --wasmFile target/wasm32-unknown-unknown/release/tic_tac_toe.wasm \
-  --nodeUrl "https://api.development.calimero.network/api/v1/shards/$shard_id-calimero-testnet/neard-rpc/" \
+  --nodeUrl "https://api.dev.calimero.network/api/v1/shards/$shard_id-calimero-testnet/neard-rpc/" \
   --networkId "$1-calimero-testnet"
