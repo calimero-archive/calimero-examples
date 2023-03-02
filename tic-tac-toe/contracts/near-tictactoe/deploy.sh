8eb67f1d7c78551eaec868d86c5c2804bc2570bd
ijerkovic <igor@calimero.network>
2023-03-02 18:19:06 +0100

    Update for deployment on demos calimero shard

--- tic-tac-toe/contracts/near-tictactoe/deploy.sh
+++ tic-tac-toe/contracts/near-tictactoe/deploy.sh
@@ -1,1 +1,1 @@
-near dev-deploy --wasmFile target/wasm32-unknown-unknown/release/tic_tac_toe.wasm --initFunction new --initArgs '{}'
+near deploy --wasmFile target/wasm32-unknown-unknown/release/tic_tac_toe.wasm --initFunction new --initArgs '{}' --accountId tictactoe.igi.testnet
