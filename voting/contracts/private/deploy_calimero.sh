7cb750298019ecf61128e03622007e05d53c0d79
Fran Domovic <fran@calimero.network>
2023-03-02 15:34:20 +0100

    fix: update nodeurl to description

--- voting/contracts/private/deploy_calimero.sh
+++ voting/contracts/private/deploy_calimero.sh
@@ -4,9 +4,14 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 destination_master_account="$1.calimero.testnet"
+near create-account \
+  "voting.$destination_master_account" \
+  --masterAccount $destination_master_account \
+  --nodeUrl "calimero-rpc-node-url" \
+  --networkId "$1-calimero-testnet" && \
 near deploy \
-  --accountId "$destination_master_account" \
+  --accountId "voting.$destination_master_account" \
   --initFunction new --initArgs '{"question": "Which blockchain is best?", "options": ["NEAR","Bitcoin"]}' \
   --wasmFile target/wasm32-unknown-unknown/release/poll.wasm \
-  --nodeUrl "https://api.dev.calimero.network/api/v1/shards/my-awesome-shard-calimero-testnet/neard-rpc/" \
-  --networkId "my-awesome-shard-calimero-testnet"
+  --nodeUrl "calimero-rpc-node-url" \
+  --networkId "$1-calimero-testnet"
