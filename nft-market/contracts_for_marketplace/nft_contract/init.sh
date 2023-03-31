0a0174dc3926a970022c0f5fe3443a96f8050125
Ivan Kuzmic <ivankuzmic@calimero.network>
2023-03-31 11:13:34 +0200

    feat: added almost all functionality

---
+++ nft-market/contracts_for_marketplace/nft_contract/init.sh
@@ -0,0 +1,1 @@
+near call <NFT_CONTARCT_SUBACOUNT_ID>.<MAIN_ACCOUNT_ID> new_default_meta '{"owner_id": "<NFT_CONTARCT_SUBACOUNT_ID>.<MAIN_ACCOUNT_ID>"}' --accountId <MAIN_ACCOUNT_ID> --networkId <SHARD_ID> --nodeUrl https://api.calimero.network/api/v1/shards/<SHARD_ID>/neard-rpc/
